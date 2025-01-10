13eccfce22d531f4c46fc221286f52edc8b7cecb
BenoitRobin <benoitrobin@me.com>
2025-01-10 14:42:00 +0100

    feat: :sparkles: reusable button - HTML + Styling
    reusable button - HTML + Styling

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,24 @@
+<script>
+    let { children, ...props } = $props();
+</script>
+
+<button {...props} >{@render children()}</button>
+
+
+<style>
+    button {
+        background-color: #000;
+        color: #fff;
+        padding: 20px 24px;
+        font-weight: normal;
+        font-size: 22px;
+        text-transform: uppercase;
+        border: 1px solid #fff;
+        transition: all .3s;
+    }
+
+    button:hover {
+        background-color: #fff;
+        color: #000;
+    }
+</style>
