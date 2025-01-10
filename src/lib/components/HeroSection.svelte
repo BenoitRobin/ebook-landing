b480ab7be3fb68685a17a51fc5d32217165b42f0
BenoitRobin <benoitrobin@me.com>
2025-01-10 14:04:36 +0100

    feat: :sparkles: Hero Section - HTML Structure
    Hero Section - HTML Structure

---
+++ src/lib/components/HeroSection.svelte
@@ -0,0 +1,25 @@
+<script>
+    import bookCover from '$assets/book_cover.png';
+    import phoneCover from '$assets/phone_cover.png';
+</script>
+
+<section class="hero">
+    <div class="hero-showcase text-center bold">
+        <h1 class="purple">MOVE TO</h1>
+        <h1 class="purple mb-xs">SPAIN</h1>
+        <h4 class="semi-bold italic">
+            There's no need to make the same mistakes I made.
+        </h4>
+        <img src={bookCover} alt="a book cover of the spain guide" class="book-cover">
+        <img src={phoneCover} alt="a wallpaper for phone with the spain guide cover" class="phone-cover">
+    </div>
+
+    <div class="hero-text white text-center">
+        <h1>THIS BOOK WILL</h1>
+        <h1>SAVE YOUR MoNEY</h1>
+        <h1 class="mb-m">TIME & NERVES</h1>
+        <p class="light-grey">
+            I moved to Spain seven years ago. The process was painful and difficult. I learned most things the hard way: finding a job, getting a visa, calculating my cost of living, choosing an apartment. That's why I wrote this e-book, to tell you know I did it. No bullshit, no stress.
+        </p>
+</div>
+</section>
